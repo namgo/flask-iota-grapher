4e7def6a053fcf6e21075a0d5ce3b6a2c32b3c9f
namgo <nahumdeltoro@riseup.net>
2017-12-16 14:45:08 -0500

    removing seconds from table

--- grapher.py
+++ grapher.py
@@ -43,7 +43,7 @@ def get_amount_trades(cursor, divideby):
         key = math.floor(document['timestamp'] / divideby)
         readable_key = datetime.fromtimestamp(
             int(math.floor((key*divideby)/1000))
-        ).strftime('%Y-%m-%d %H:%M:%S')
+        ).strftime('%Y-%m-%d %H:%M')
         if document['amount'] > 0:
             try:
                 amount_buy[key]
