d85effe8cb70b2d30ab61f8f1d236c4f3a59a56c
namgo <nahumdeltoro@riseup.net>
2017-12-18 15:53:38 -0500

    fixing label dict object

--- mpl_grapher.py
+++ mpl_grapher.py
@@ -167,8 +167,8 @@ def show_amt_div_transactions():
 
     fig = Figure()
     ax = fig.add_subplot(111)
-    buy_plot, = ax.plot(buy_dates, y_buy, 'b-', legend="Buys")
-    sell_plot, = ax.plot(sell_dates, y_sell, 'r-', legend="Sells")
+    buy_plot, = ax.plot(buy_dates, y_buy, 'b-', label="Buys")
+    sell_plot, = ax.plot(sell_dates, y_sell, 'r-', label="Sells")
     ax.legend(handles=[buy_plot, sell_plot])
     ax.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d %H:%M'))
     fig.autofmt_xdate()
