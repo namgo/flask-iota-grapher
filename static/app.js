3d7b370a6f423a979d483ed4c01769c6ad980daa
namgo <nahumdeltoro@riseup.net>
2017-12-18 11:16:57 -0500

    moving js to js file

--- static/app.js
+++ static/app.js
@@ -1,25 +1,20 @@
-var graphApp = angular.module('graphApp', []);
-graphApp.controller('GraphController', function($scope, $http, $sce) {
-    var d = new Date();
-    console.log(d.getTime())
-    $scope.startTime = d.getTime()-(2000*60);
-    $scope.endTime = d.getTime()+1000;
-    $scope.groupBySeconds = 30;
-    
-    $scope.renderHtml = function(html_code)
-    {
-	return $sce.trustAsHtml(html_code);
-    };
-    $scope.getSells = function () {
+
+var imageApp = angular.module('ImageApp', []);
+imageApp.controller('ImageController', function($scope, $http) {
+    $scope.sendGoBack = function() {
+	var params = $.param({
+	    goback: $scope.goback,
+	    interval: $scope.interval
+	});
 	$http({
-	    method: 'GET',
-	    url: 'http://'+window.location.hostname+':5000/api/sells?min='+
-		$scope.startTime+'&max='+$scope.endTime+'&divideby='+
-		$scope.groupBySeconds
-	}).then(function(data) {
-	    $scope.sellGraph = data.data;
-	},function(error) {
-	    console.log(error);
+	    url: '/amt.png',
+	    method: "GET",
+	    params: params
+	}).then(function(data, status, headers, config) {
+	    $scope.ImageResponse = data;
+	}, function (response) {
+	    $scope.ImageResponse = response;
 	})
     }
+    
 })
