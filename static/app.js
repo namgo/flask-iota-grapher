30923fd84733bb7204b4a1b7f030178a9d770612
namgo <nahumdeltoro@riseup.net>
2017-12-18 11:36:49 -0500

    fixng param name

--- static/app.js
+++ static/app.js
@@ -1,13 +1,15 @@
 
 var imageApp = angular.module('ImageApp', []);
 imageApp.controller('ImageController', ['$scope', '$http', function($scope, $http) {
+    $scope.goback = 1440;
+    $scope.interval_back = 3600;
     $scope.sendGoBack = function() {
 	$http({
 	    url: '/amt.png',
 	    method: "GET",
 	    params: {
 		goback: $scope.goback,
-		interval: $scope.interval
+		interval: $scope.interval_back
 	    }
 	}).then(function(data, status, headers, config) {
 	    $scope.ImageResponse = data;
