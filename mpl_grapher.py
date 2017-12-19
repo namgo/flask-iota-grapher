42050da782b7e0ad399042ba44374a1f34378adf
namgo <nahumdeltoro@riseup.net>
2017-12-19 17:55:43 -0500

    didn't submit full url for mongodb previously

--- mpl_grapher.py
+++ mpl_grapher.py
@@ -25,7 +25,7 @@ from pymongo import MongoClient
 app = Flask(__name__)
 
 client = MongoClient(
-    'mongodb://localhost:27017'
+    'mongodb://localhost:27017/iotatracker'
 )
 
 db = client.iotatracker
